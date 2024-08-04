6d9cfbf56fcee5e3d80f2b92a2fda901fa57efd9
Z-ajamy <abdorahman0283@gmail.com>
2024-08-04 12:59:26 +0300

    0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,34 @@
-#!/usr/bin/python3
-"""
-Index route for the API
-"""
-
+#!/usr/bin/env python3
+"""Defines a status route for the HolbertonBnB API."""
 from flask import jsonify
-from api.v1.views import app_views
+from flasgger import swag_from
 from models import storage
+from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route("/status")
+@swag_from("../apidocs/status/status.yml")
 def status():
-    """Return the status of the API"""
+    """Returns the server status.
+
+    Returns:
+        JSON object with the current server status.
+    """
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])
+
+@app_views.route("/stats")
+@swag_from("../apidocs/stats/stats.yml")
 def stats():
-    """Return the number of each object type"""
-    object_counts = {
+    """Retrives the count of each object type.
+
+    Returns:
+        JSON object with the number of objects by type."""
+    return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
-    }
-    return jsonify(object_counts)
+    })
