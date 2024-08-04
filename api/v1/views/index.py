f582a0014568da05ade145fbebd91af035e7c4bc
Z-ajamy <abdorahman0283@gmail.com>
2024-08-04 12:26:54 +0300

    0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,22 @@ Index route for the API
 
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Return the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Return the number of each object type"""
+    object_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(object_counts)
