196caf5450d9568fe19d83e0b0e9cbbcc6d23f97
Z-ajamy <abdorahman0283@gmail.com>
2025-12-21 22:54:07 +0200

    DocStr

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+"""
+Main Flask Application for AirBnB Clone v3 API
+"""
+from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def after_app(error):
+def tear_down(error):
+    """
+    Closes the storage session after each request
+    """
     storage.close()
-
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+
+    app.run(host=host, port=int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,9 @@ Index view for the API
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route("/status")
-def statusok():
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def status():
+    """
+    Returns the status of the API
+    """
     return jsonify({"status": "OK"})
