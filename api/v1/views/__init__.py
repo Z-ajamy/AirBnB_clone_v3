0719aeb80d4d6e9a54fdec2c0b97b400b6cfe174
Z-ajamy <abdorahman0283@gmail.com>
2026-01-19 15:48:27 +0200

    AirBnB_clone CORS Handeling

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,9 +10,9 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from api.v1.vews.users import *
-from api.v1.vews.places import *
-from api.v1.vews.review import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
 
 
 
